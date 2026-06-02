c96897a98afb183151b01836cead9297ade045b1
ragomezr <rene.alejandro.gomez.rodriguez@usherbrooke.ca>
2026-06-02 15:52:01 -0400

    3:51 fix

--- main.py
+++ main.py
@@ -10,29 +10,28 @@ from google.transit import gtfs_realtime_pb2
 URL = "https://exo.chrono-saeiv.com/api/opendata/v1/STS/vehicleposition?token=ed02f9a16ccc44f1b3ee37d704db5bfd"
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
+
 def obtener_conexion():
-    # Si estás probando local, puedes poner tu cadena de conexión directa aquí temporalmente
     if not DATABASE_URL:
         raise ValueError("La variable de entorno DATABASE_URL no está configurada.")
     return psycopg2.connect(DATABASE_URL)
 
-    
-
 def crear_db():
     conn = obtener_conexion()
     cursor = conn.cursor()
+    # Cambiado a SERIAL (Postgres) y usando cursor.execute
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS snapshots (
-            id          INTEGER PRIMARY KEY AUTOINCREMENT,
-            timestamp   TEXT,
-            vehicle_id  TEXT,
-            route_id    TEXT,
-            lat         REAL,
-            lon         REAL,
-            speed_kmh   REAL,
-            bearing     REAL,
-            stop_id     TEXT,
-            status      INTEGER
+            id SERIAL PRIMARY KEY,
+            timestamp TEXT,
+            vehicle_id TEXT,
+            route_id TEXT,
+            lat REAL,
+            lon REAL,
+            speed_kmh REAL,
+            bearing REAL,
+            stop_id TEXT,
+            status INTEGER
         )
     """)
     conn.commit()
@@ -53,24 +52,17 @@ def guardar_snapshot():
         if entity.HasField('vehicle'):
             v = entity.vehicle
             timestamp = datetime.fromtimestamp(v.timestamp, tz=timezone.utc).isoformat()
-
+            # Cambiado a %s (Postgres) y usando el cursor correctamente
             cursor.execute("""
-                INSERT INTO snapshots 
-                (timestamp, vehicle_id, route_id, lat, lon, speed_kmh, bearing, stop_id, status)
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+                INSERT INTO snapshots (timestamp, vehicle_id, route_id, lat, lon, speed_kmh, bearing, stop_id, status)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
             """, (
-                timestamp,
-                v.vehicle.id,
-                v.trip.route_id,
-                v.position.latitude,
-                v.position.longitude,
-                round(v.position.speed * 3.6, 1),
-                v.position.bearing,
-                v.stop_id,
-                v.current_status
+                timestamp, v.vehicle.id, v.trip.route_id, v.position.latitude, v.position.longitude,
+                round(v.position.speed * 3.6, 1) if v.position.speed else 0.0, 
+                v.position.bearing, v.stop_id, v.current_status
             ))
-            count+= 1
-
+            count += 1
+            
     conn.commit()
     cursor.close()
     conn.close()
